f8a9898053853458f0f0bad8c0ede16a3f1ee319
AHBRIJESH <ahbrijesh2004@gmail.com>
2025-11-24 10:05:24 +0530

    Edit

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
-import joblib
+import pickle
 import numpy as np
 
 app = Flask(__name__)
-CORS(app)  # <== Allow requests from frontend
+CORS(app)
 
-model = joblib.load("model.pkl")
+model = pickle.load(open("model.pkl", "rb"))
+
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.get_json()
-    x_val = float(data["x"])
-    x_arr = np.array([[x_val]])
-    y_pred = model.predict(x_arr)[0]
-    return jsonify({"prediction": float(y_pred)})
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    x = float(data["x"])
+    prediction = model.predict([[x]])[0]
+    return jsonify({"prediction": float(prediction)})
